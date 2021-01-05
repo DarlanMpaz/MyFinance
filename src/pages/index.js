3baf756b92f733adb886a042e3faa2de53841e79
Darlan Paz <darlan.mpaz@gmail.com>
2021-01-05 20:04:13 -0300

    Front-End /newTransaction pronto

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,61 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import axios from 'axios'
+import { useState } from 'react'
+import { FiPlusCircle, FiTrash, FiArrowUp, FiArrowDown } from 'react-icons/fi'
 
-import styles from '../styles/Home.module.css'
+export default function Home() {
+
+  const [ transactions, setTransactions ] = useState([])
+
+  let incomeIcon = 'FiArrowUp'
+
+  const response = axios.get('/api/transaction').then(resp => {
+    setTransactions(resp.data)
+  })
 
   return (
 
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>MyFinance</title>
+        <title>MyFinance - Nova Transação</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-          <Link href="/newTransaction"><a>- Nova Transação</a></Link>
-      </main>
-    </div>
+      <div className="header-background"></div>
+
+      <div className="container">
+
+          <header>
+              <h1><strong>My</strong>Finance</h1>
+          </header>
+
+          <main>
+
+            {transactions.map( transaction => (
+
+              <div className="transaction" key={transaction._id}>
+                <div className="description">
+                  <Link href="#"><a className="footer-icon"><FiTrash /></a></Link>
+                  <p>{transaction.description}</p>
+                </div>
+                <div className="amount">
+                  <p>{transaction.amount}</p>
+                  {transaction.income === 'income'? incomeIcon = 'FiArrowUp' : incomeIcon = 'FiArrowDown' }
+                </div>
+                <p>{transaction.income}</p>
+              </div>
+
+            ) )}
+
+            <Link href="/newTransaction"><a>- Nova Transação</a></Link>
+          </main>
+
+          <footer> 
+              <Link href="/newTransaction"><a className="footer-icon"><FiPlusCircle /><p>Nova</p></a></Link>
+          </footer>
+      </div>
+    </>
 
   )
 }
--- src/pages/newTransaction.js
+++ src/pages/newTransaction.js
@@ -2,14 +2,17 @@ import {useState} from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import axios from 'axios'
+import { useRouter } from 'next/router'
+import { FiArrowLeftCircle } from 'react-icons/fi'
 
-import styles from '../styles/Home.module.css'
 
 export default function newTransaction() {
 
-    const [description, setDescription] = useState(null)
-    const [amount, setAmount] = useState(null)
-    const [income, setIncome] = useState(null)
+    const [description, setDescription] = useState('')
+    const [amount, setAmount] = useState('')
+    const [income, setIncome] = useState('')
+
+    const router = useRouter()
 
     const handleSubmit = async (event) => {
         event.preventDefault()
@@ -22,44 +25,63 @@ export default function newTransaction() {
            alert(error.response.data.error) 
         }
 
+        router.push('/')
+
     }
 
   return (
 
-    <div className={styles.container}>
+    <>
         <Head>
             <title>MyFinance - Nova Transação</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <main className={styles.main}>
-            <h1>Nova transação</h1>
-            <Link href="/"><a>- Voltar para a Home</a></Link>
-        </main>
+        <div className="header-background"></div>
 
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="text" 
-                value={description} 
-                onChange={(e) => setDescription(e.target.value)}
-                placeholder="Descrição"
-            />
+        <div className="container">
 
-            <input 
-                type="number" 
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                placeholder="Valor"
-            />
+            <header>
+                <h1><strong>My</strong>Finance</h1>
+            </header>
 
-            <h2>Tipo da despesa</h2>
-            <div onClick={() => setIncome(true)}>Receita</div>
-            <div onClick={() => setIncome(false)}>Despesa</div>
+            <main className="card shadow">
 
-            <button type="submit">Feito</button>
-        </form>
+                <form onSubmit={handleSubmit}>
 
-    </div>
+                    <div className="card-header">
+                        <h3>Nova transação</h3>
+                    </div>
+                    
+                    <div className="card-main">
+
+                        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Descrição" />
+
+                        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Valor"/>
+
+                        {/* <label for="income">Tipo de operação</label> */}
+
+                        <select id="income" onChange={(e) => setIncome(e.target.value)}>
+                            <option disabled selected>Tipo</option>
+                            <option value="income">Receita</option>
+                            <option value="expense">Despesa</option>
+                        </select>
+
+                    </div>
+
+                    <div className="card-footer">
+                        <button className="button" type="submit">Feito</button>
+                    </div>
+
+                </form>
+            </main>
+
+            <footer> 
+                <Link href="/"><a className="footer-icon"><FiArrowLeftCircle /><p>Voltar</p></a></Link>
+            </footer>
+        </div>
+
+    </>
 
   )
 }
